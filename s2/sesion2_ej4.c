a05388969b62b91345c57bc2b822333246962bbe
Umar <mrumar628@gmail.cm>
2024-09-25 16:53:33 +0200

    main

---
+++ s2/sesion2_ej4.c
@@ -0,0 +1,21 @@
+/*
+** Fichero: sesion2_ej4.c
+** Autor: Umar Mohammad Riaz
+** Fecha: 25/07/2024
+**
+** Descripcion: Ejercicio 4 de la sesión 2 de laboratorio.
+**
+*/
+
+#include <stdio.h>
+
+#define PI 3.14
+
+int main() {
+    
+
+    
+
+    return 0;
+}
+
--- s2/sesion2_ej5.c
+++ s2/sesion2_ej5.c
@@ -1,3 +1,13 @@
+/*
+** Fichero: sesion2_ej5.c
+** Autor: Umar Mohammad Riaz
+** Fecha: 25/07/2024
+**
+** Descripcion: Ejercicio 5 de la sesión 2 de laboratorio.
+**
+*/
+
+
 #include <stdio.h>
 
 
@@ -8,6 +18,10 @@ int main()
    printf("Introduzca dos valores para las variables a y b: ");
    scanf("%d %d%*c", &a, &b);
 
-   printf("Variable a: %d\n", b);
-   printf("Variable b: %d\n", a);
+   aux = b; // aux guarda el valor de b
+   b = a; // com aux te el valor de b, ara si, b guarda el valor de a
+   a = aux; // i finalmente el valor guardat a aux passa a a
+
+   printf("Variable a: %d\n", a);
+   printf("Variable b: %d\n", b);
 };
