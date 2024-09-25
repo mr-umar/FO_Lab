f84b939f68d44014734bb9fb3e529393c0aafc06
Umar <mrumar628@gmail.cm>
2024-09-25 16:07:57 +0200

    main

--- s2/sesion2_ej1.c
+++ s2/sesion2_ej1.c
@@ -1,42 +1,47 @@
 /*
-Umar Mohammad Riaz
-S2 - Ej. 1 
-*/
+** Fichero: sesion2_ej1.c
+** Autor: Umar Mohammad Riaz
+** Fecha: 25/07/2024
+**
+** Descripción: Ejercico 1 de la sesión 2 de labotario.
+**
+*/ 
 
 # include <stdio.h>
 
-int k=7, resi; 
-char c='a', resc; 
-float f=5.5, g=-3.25, resf;
-
 int main(){
 
-    resi = -2 + k; /* la variable resi vale _______ */  
-    printf("la variable resi vale %d", resi);
+    int k=7, resi; 
+    char c='a', resc; 
+    float f=5.5, g=-3.25, resf;
 
-    resi = resi + 2; /* la variable resi vale _______ */ 
-    printf("la variable resi vale %d", resi);
 
-    resc = c; /* la variable resc vale _______ */ 
-    printf("la variable resc vale %d", resc);
+    resi = -2 + k; /* la variable resi vale 5 */  
+    printf("la variable resi vale %d\n", resi);
 
-    resi = k%5; /* la variable resi vale _______ */ 
-    printf("la variable resi vale %d", resi);
+    resi = resi + 2; /* la variable resi vale 7 */ 
+    printf("la variable resi vale %d\n", resi);
 
-    resf = (f - g)/2; /* la variable resf vale _______ */ 
-    printf("la variable resf vale %d", resf);
+    resc = c; /* la variable resc vale a */ 
+    printf("la variable resc vale %c\n", resc);
 
-    resi = resi*(k - 3); /* la variable resi vale _______ */ 
-    printf("la variable resi vale %d", resi);
+    resi = k%5; /* la variable resi vale 2 */ 
+    printf("la variable resi vale %d\n", resi);
 
-    resf = k/(resi - 2); /* la variable resf vale _______ */ 
-    printf("la variable resf vale %d", resf);
+    resf = (f - g)/2; /* la variable resf vale 4.3 */ 
+    printf("la variable resf vale %f\n", resf);
 
-    resf = f/(resi - 2); /* la variable resf vale _______ */ 
-    printf("la variable resf vale %d", resf);
+    resi = resi*(k - 3); /* la variable resi vale 8 */ 
+    printf("la variable resi vale %d\n", resi);
 
-    resi = 2*(k - 3)%3/2;  /* la variable resi vale _______ */
-    printf("la variable resi vale %d", resi);
+    resf = k/(resi - 2); /* la variable resf vale 1.0 */ 
+    printf("la variable resf vale %f\n", resf);
+
+    resf = f/(resi - 2); /* la variable resf vale 0.91 */ 
+    printf("la variable resf vale %f\n", resf);
+
+    resi = 2*(k - 3)%3/2;  /* la variable resi vale 1 */
+    printf("la variable resi vale %d\n", resi);
     
     return 0;
 }
---
+++ s2/sesion2_ej2.c
@@ -0,0 +1,32 @@
+/*
+** Fichero: sesion2_ej2.c
+** Autor: Umar Mohammad Riaz
+** Fecha: 25/07/2024
+**
+** Descripción: Ejercico 2 de la sesión 2 de labotario.
+**
+*/ 
+
+# include <stdio.h>
+
+int main(){
+
+    int expresion;
+
+    expresion = 5/2 + 20%6;
+    printf("La expresion 5/2 + 20%%6 devuelve: %d", expresion);
+
+    /*
+    5/2 + 20%6          
+4*6/2 - 15/2  
+5*15/2/(4 - 2)  
+8 == 16 || 7 != 4 && 4 < 1  
+(4*3 < 6 || 3 > 5 - 2) && 3 + 2 < 12  
+2 || 0  
+1 && 0 
+    */
+
+
+
+    return 0;
+}
