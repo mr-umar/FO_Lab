b9c939425c6a10666077b0e68bea6be7a92fbf6b
Umar <mrumar628@gmail.cm>
2024-09-25 16:20:11 +0200

    MAIN

--- s2/sesion2_ej2.c
+++ s2/sesion2_ej2.c
@@ -3,7 +3,7 @@
 ** Autor: Umar Mohammad Riaz
 ** Fecha: 25/07/2024
 **
-** Descripción: Ejercico 2 de la sesión 2 de laboratorio.
+** Descripción: Ejercicio 2 de la sesión 2 de laboratorio.
 **
 */
 
@@ -13,32 +13,31 @@ int main() {
 
     int expresion;
 
-    // Primera expresión: 5/2 + 20%6
-    expresion = 5 / 2 + 20 % 6;
+    expresion = 5 / 2 + 20 % 6; // 4
     printf("La expresion 5/2 + 20%%6 devuelve: %d\n", expresion); // Resultado esperado: 4
 
-    // Segunda expresión: 4*6/2 - 15/2
-    expresion = 4 * 6 / 2 - 15 / 2;
+
+    expresion = 4 * 6 / 2 - 15 / 2; // 5
     printf("La expresion 4*6/2 - 15/2 devuelve: %d\n", expresion); // Resultado esperado: 6
 
-    // Tercera expresión: 5*15/2/(4 - 2)
-    expresion = 5 * 15 / 2 / (4 - 2);
+
+    expresion = 5 * 15 / 2 / (4 - 2); // 18
     printf("La expresion 5*15/2/(4 - 2) devuelve: %d\n", expresion); // Resultado esperado: 18
 
-    // Cuarta expresión: 8 == 16 || 7 != 4 && 4 < 1
-    expresion = 8 == 16 || 7 != 4 && 4 < 1;
+
+    expresion = 8 == 16 || 7 != 4 && 4 < 1; // 0 (False)
     printf("La expresion 8 == 16 || 7 != 4 && 4 < 1 devuelve: %d\n", expresion); // Resultado esperado: 1 (true)
 
-    // Quinta expresión: (4*3 < 6 || 3 > 5 - 2) && 3 + 2 < 12
-    expresion = (4 * 3 < 6 || 3 > 5 - 2) && 3 + 2 < 12;
+
+    expresion = (4 * 3 < 6 || 3 > 5 - 2) && 3 + 2 < 12; // 0
     printf("La expresion (4*3 < 6 || 3 > 5 - 2) && 3 + 2 < 12 devuelve: %d\n", expresion); // Resultado esperado: 1 (true)
 
-    // Sexta expresión: 2 || 0
-    expresion = 2 || 0;
+
+    expresion = 2 || 0; // 1 (True)
     printf("La expresion 2 || 0 devuelve: %d\n", expresion); // Resultado esperado: 1 (true)
 
-    // Séptima expresión: 1 && 0
-    expresion = 1 && 0;
+
+    expresion = 1 && 0; // 0
     printf("La expresion 1 && 0 devuelve: %d\n", expresion); // Resultado esperado: 0 (false)
 
     return 0;
---
+++ s2/sesion2_ej3.c
@@ -0,0 +1,9 @@
+/*
+** Fichero: sesion2_ej3.c
+** Autor: Umar Mohammad Riaz
+** Fecha: 25/07/2024
+**
+** Descripción: Ejercicio 3 de la sesión 2 de laboratorio.
+**
+*/
+
