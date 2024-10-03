149a638fe019694272cce16d273a1be5b7af0013
Umar <mrumar628@gmail.cm>
2024-10-03 15:41:06 +0200

    main

--- s2/sesion2_ej8.c
+++ s2/sesion2_ej8.c
@@ -12,6 +12,26 @@
 #define PI 3.1415
 
 int main(){
-    
+
+    char tipoFigura;
+    float datoA, datoB, area;
+
+    printf("Introduzca la descripcion de la figura: ");
+    scanf("%c %f %f", &tipoFigura, &datoA, &datoB);
+
+    if (tipoFigura == 't' || tipoFigura == 'T'){
+        area = (datoA * datoB) / 2;
+
+        printf("Area del Triangulo = %f\n", area);
+    } else if (tipoFigura == 'c' || tipoFigura == 'C') {
+        area = PI * datoA * datoA;
+
+        printf("Area del Circulo = %f\n", area);
+    } else {
+        printf("El tipo de la figura es incorrecto!\n");
+    };
+
+    return 0;
+
 }
 
