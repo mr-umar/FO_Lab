21b243647a53efabb844d9bcad09318abaaa85ff
Umar <mrumar628@gmail.cm>
2024-10-09 16:44:27 +0200

    b

--- s3/sesion3_ej6b.c
+++ s3/sesion3_ej6b.c
@@ -6,3 +6,22 @@
 ** Descripción: Ejercicio 6b de la sesión 3 de laboratorio.
 **
 */
+
+#include <stdio.h>
+
+int main() {
+    double x, resultado = 1.0;
+    unsigned int n;
+    int i;  
+
+    printf("Introduzca el valor de x (real): ");
+    scanf("%lf", &x);
+
+    printf("Introduzca el valor de n (natural): ");
+    scanf("%u", &n);
+
+
+
+    
+    return 0;
+}
