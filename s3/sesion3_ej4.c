2e9126bbd48a31b66b1b6759a27b4e38f75efc83
Umar <mrumar628@gmail.cm>
2024-10-09 15:51:18 +0200

    rg

---
+++ s3/sesion3_ej4.c
@@ -0,0 +1,18 @@
+/*
+** Fichero: sesion3_ej4.c
+** Autor: Umar Mohammad Riaz
+** Fecha: 09/10/2024
+**
+** Descripción: Ejercicio 4 de la sesión 3 de laboratorio.
+**
+*/
+
+
+#include <stdio.h>
+
+int main() {
+
+    
+
+    return 0;
+}
