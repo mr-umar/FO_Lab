1dffda1904890d34ef0b59f9120005a41f75ebb2
Umar <mrumar628@gmail.cm>
2024-10-23 16:53:15 +0200

    Falta completar el 4

--- s4/sesion4_ej4.c
+++ s4/sesion4_ej4.c
@@ -6,14 +6,6 @@
 ** Descripción: Ejercicio 4 de la sesión 4 de laboratorio.
 **
 */
-/*
-** Fichero: sesion4_ej4.c
-** Autor: Umar Mohammad Riaz
-** Fecha: 23/10/2024
-**
-** Descripción: Ejercicio 4 de la sesión 4 de laboratorio.
-**
-*/
 
 #include <stdio.h>
 
@@ -25,29 +17,30 @@ typedef struct {
 int main() {
     tfraccion fraccion1, fraccion2, resultado;
 
-    // Entrada de las fracciones
     printf("Fraccion 1 (formato x/y): ");
     scanf("%d/%d", &fraccion1.num, &fraccion1.den);
 
     printf("Fraccion 2 (formato x/y): ");
     scanf("%d/%d", &fraccion2.num, &fraccion2.den);
 
-    // Suma: (a/b) + (c/d) = (a*d + b*c) / (b*d)
+    printf("\nRESULTADOS:\n");
+
+    // Suma
     resultado.num = fraccion1.num * fraccion2.den + fraccion1.den * fraccion2.num;
     resultado.den = fraccion1.den * fraccion2.den;
     printf("Suma: %d/%d + %d/%d = %d/%d\n", fraccion1.num, fraccion1.den, fraccion2.num, fraccion2.den, resultado.num, resultado.den);
 
-    // Resta: (a/b) - (c/d) = (a*d - b*c) / (b*d)
+    // Resta
     resultado.num = fraccion1.num * fraccion2.den - fraccion1.den * fraccion2.num;
     resultado.den = fraccion1.den * fraccion2.den;
     printf("Resta: %d/%d - %d/%d = %d/%d\n", fraccion1.num, fraccion1.den, fraccion2.num, fraccion2.den, resultado.num, resultado.den);
 
-    // Multiplicación: (a/b) * (c/d) = (a*c) / (b*d)
+    // Multiplicació
     resultado.num = fraccion1.num * fraccion2.num;
     resultado.den = fraccion1.den * fraccion2.den;
     printf("Multiplicacion: %d/%d * %d/%d = %d/%d\n", fraccion1.num, fraccion1.den, fraccion2.num, fraccion2.den, resultado.num, resultado.den);
 
-    // División: (a/b) / (c/d) = (a*d) / (b*c)
+    // Divisió
     resultado.num = fraccion1.num * fraccion2.den;
     resultado.den = fraccion1.den * fraccion2.num;
     printf("Division: %d/%d / %d/%d = %d/%d\n", fraccion1.num, fraccion1.den, fraccion2.num, fraccion2.den, resultado.num, resultado.den);
