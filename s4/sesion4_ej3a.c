e70b5ccad062b6fcbff3edf2779f184ef098ea1b
Umar <92973740+mr-umar@users.noreply.github.com>
2024-10-23 16:16:22 +0200

    Update sesion4_ej3a.c

--- s4/sesion4_ej3a.c
+++ s4/sesion4_ej3a.c
@@ -31,3 +31,33 @@ int main() {
     return 0;
 }
 
+/*
+** Fichero: sesion4_ej3a.c
+** Autor: Umar Mohammad Riaz
+** Fecha: 23/10/2024
+**
+** Descripción: Ejercicio 3a de la sesión 4 de laboratorio.
+**
+*/
+
+#include <stdio.h>
+
+typedef struct {
+    unsigned int hh;  // Horas
+    unsigned int mm;  // Minutos
+    unsigned int ss;  // Segundos
+} texpr_horaria;
+
+int main() {
+    texpr_horaria tiempo;
+    unsigned int totalSegundos;
+
+    printf("Expresion horaria (hh mm ss): ");
+    scanf("%u %u %u", &tiempo.hh, &tiempo.mm, &tiempo.ss);
+
+    totalSegundos = tiempo.hh * 3600 + tiempo.mm * 60 + tiempo.ss;
+
+    printf("Segundos: %u\n", totalSegundos);
+
+    return 0;
+}
