49802ecccd58b828b0c61d197c5f25993f93f99e
Umar <92973740+mr-umar@users.noreply.github.com>
2024-10-23 14:28:22 +0200

    Update sesion3_ej12.c

--- s3/sesion3_ej12.c
+++ s3/sesion3_ej12.c
@@ -1,14 +1,52 @@
+/*
+** Fichero: sesion3_ej12.c
+** Autor: Umar Mohammad Riaz
+** Fecha: 16/10/2024
+**
+** Descripción: Ejercicio 12 de la sesión 3 de laboratorio.
+**
+*/
+
 #include <stdio.h>
-#include "colours.h"	
-#include "azar.h"	
+#include "colours.h"
+#include "azar.h"
 
-int main()
-{
- // Declare aqui las variables necesarias
+int main() {
+    int posicionRaton = 29, posicionGato = 0;
+    int dadoRaton, dadoGato;
+    int pista = 60;
 
+    inicializar_azar();
 
-  inicializar_azar();		// Inicializa la semilla del generador de numeros aleatorio
+    while (1) {
+        // Turno del ratón
+        dadoRaton = numero_al_azar(6) + 1;
+        posicionRaton = (posicionRaton + dadoRaton) % pista;
+        cambiar_color(GREEN);
+        printf("Raton: He sacado un %d. Voy a la casilla %d\n", dadoRaton, posicionRaton);
 
- // Escriba a partir de aqui el codigo del programa
+        if (posicionRaton == posicionGato) {
+            cambiar_color(RED);
+            printf("Gato: He ganado!!!!\n");
+            cambiar_color(DEFAULT);
+            break;
+        }
 
+        // Turno del gato
+        dadoGato = (numero_al_azar(6) + 1) * 2;
+        posicionGato = (posicionGato + dadoGato) % pista;
+        cambiar_color(RED);
+        printf("Gato: He sacado un %d. Voy a la casilla %d\n", dadoGato, posicionGato);
+
+        if (posicionGato == posicionRaton) {
+            cambiar_color(RED);
+            printf("Gato: He ganado!!!!\n");
+            cambiar_color(DEFAULT);
+            break;
+        }
+
+        cambiar_color(DEFAULT);  // Restablecer el color después de cada ciclo
+    }
+
+    return 0;
 }
