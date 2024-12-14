2f2d85f91e23af89607a6e7c34093730abcf4e97
Umar Mohammad <mrumar628@gmail.com>
2024-12-14 16:05:37 +0100

    Add initial implementation of lab12g71.c and test.c with size of int example

---
+++ Examen Lab Ejercicios/lab12g71.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define MAX_NOM 20  
+#define MAX_ALM 15  
+
+typedef struct {  
+char nombre[MAX_NOM];  
+    int edad;  
+    char sexo;  /* Valores posibles para el campo sexo v, m, para indicar que es varon o mujer */  
+} talumno;  
+
+
+typedef struct {  
+    int nelems;  
+    talumno alms[MAX_ALM];  
+} tlista;
+
+
+void leer_alumno() {
+    
+}
+
+
+
+talumno leer_alumno();
+
+
+
+int main() {
+
+
+
+}
---
+++ Examen Lab Ejercicios/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int pr = sizeof(int); // Almacena el tamaño de un entero en bytes
+
+    printf("El tamaño de un entero en este sistema es: %d bytes\n", pr); // Imprime el valor
+
+    return 0;
+}
